cf4f389f31e7197d1405becc2c02966ff6315794
antoniokho13 <khoantonio03@gmail.com>
2024-11-29 15:37:20 +0800

    Update: Connect Logged-In User to Recipes

--- FoodieRecipeSharing_Final/Recipes/views.py
+++ FoodieRecipeSharing_Final/Recipes/views.py
@@ -1,3 +1,5 @@
+from django.forms import BaseModelForm
+from django.http import HttpResponse
 from django.urls import reverse_lazy  # type: ignore
 from django.views.generic import ListView, CreateView, UpdateView, DeleteView  # type: ignore
 from django.contrib.auth.mixins import LoginRequiredMixin  # type: ignore # Add this import
@@ -10,22 +12,34 @@ class RecipePostListView(ListView):
     template_name = 'recipes/recipePost_list.html'
     context_object_name = 'posts'
 
-class RecipePostCreateView(CreateView):  # Add LoginRequiredMixin
+class RecipePostCreateView(LoginRequiredMixin, CreateView):  # Add LoginRequiredMixin
     model = RecipePost
     form_class = RecipePostForm
     template_name = 'recipes/recipePost_create.html'
     success_url = reverse_lazy('recipe_list')
 
-class RecipePostUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.user=self.request.user
+        return super().form_valid(form)
+
+class RecipePostUpdateView(LoginRequiredMixin, UpdateView):
     model = RecipePost
     form_class = RecipePostForm
     template_name = 'recipes/recipePost_update.html'
     success_url = reverse_lazy('recipe_list')
 
-class RecipePostDeleteView(DeleteView):
+    def get_queryset(self):
+        # Restrict updates to recipes owned by the logged-in user
+        return self.model.objects.filter(user=self.request.user)
+
+class RecipePostDeleteView(LoginRequiredMixin, DeleteView):
     model = RecipePost
     template_name = 'recipes/recipePost_confirm_delete.html'
     success_url = reverse_lazy('recipe_list')
+
+    def get_queryset(self):
+        # Restrict deletions to recipes owned by the logged-in user
+        return self.model.objects.filter(user=self.request.user)
 
 def cuisine_view(request):
     # Get all unique categories from the RecipePost model
